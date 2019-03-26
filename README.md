3454a3b2bd6d5034e659939dca13bd3b8be7e676
Damir Cavar <dcavar@me.com>
2019-03-26 17:56:20 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Xrenner-JSON-NLP
-Xrenner wrapper for JSON-NLP.
+# Xrenner to JSON-NLP
+
+# NLTK-JSON-NLP
+
+(C) 2019 by [Damir Cavar], Oren Baldinger, Maanvitha Gongalla, Anurag Kumar, Murali Kammili
+
+Brought to you by the [NLP-Lab.org]!
+
+
+## Introduction
+
+[Xrenner] wrapper for [JSON-NLP].
+
+
+
+
+[Damir Cavar]: http://damir.cavar.me/ "Damir Cavar"
+[NLP-Lab.org]: http://nlp-lab.org/ "NLP-Lab.org"
+[JSON-NLP]: https://github.com/dcavar/JSON-NLP "JSON-NLP"
+[Flair]: https://github.com/zalandoresearch/flair "Flair"
+[spaCy]: https://spacy.io/ "spaCy"
+[NLTK]: http://nltk.org/ "Natural Language Processing Toolkit"
+[Polyglot]: https://github.com/aboSamoor/polyglot "Polyglot" 
+[Xrenner]: https://github.com/amir-zeldes/xrenner "Xrenner"
+[CONLL-U]: https://universaldependencies.org/format.html "CONLL-U"
---
+++ setup.py
@@ -0,0 +1,26 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xrennerjsonnlp",
+    version="0.0.1",
+    author="Damir Cavar, Oren Baldinger, Maanvitha Gongalla, Anurag Kumar, Murali Kammili",
+    author_email="damir@cavar.me",
+    description="The Python Xrenner JSON-NLP package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dcavar/Xrenner-JSON-NLP",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'xrenner>=2.0.2.0',
+        'pyjsonnlp>=0.2.1'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
